a975f40e8eb3cd0569255b927c129f26c92587ea
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2019-03-09 22:39:14 +0300

    Improved script

---
+++ Улучшенная версия!parsing(python)-hh.com.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+headers = {'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'}
+base_url = 'https://hh.ru/search/vacancy?search_period=3&text=python&area=1&page=0'
+out = 'parser_python.txt'
+
+
+def hh_parse(base_url, headers,):
+    urls = []
+    urls.append(base_url)
+    session = requests.session()
+    request = session.get(base_url, headers=headers)
+    if request.status_code == 200:
+        soup = bs(request.content, 'lxml')
+        try:
+            pagination = soup.find_all("a", attrs={"data-qa": "pager-page"})
+            count = int(pagination[-1].text)
+            for i in range(count):
+                url = f"https://hh.ru/search/vacancy?search_period=3&text=python&area=1&page={i}"
+                if url not in urls:
+                    urls.append(url)
+
+        except:
+            pass
+    for url in urls:
+        request = session.get(url, headers=headers)
+        soup = bs(request.content, 'lxml')
+        divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
+        parser_python_file = open(out, 'a')
+        for div in divs:
+            try:
+                title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+                compensation = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'})
+                if compensation == None:
+                    compensation = 'Не указанно'
+                else:
+                    compensation = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'}).text
+                href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
+                company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+                text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
+                text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+                content = 'Условия:\n' + text1 + '\nТребования к кандидату:\n' + text2
+                all = title + '\n' + compensation + '\n' + href + '\n' + company + '\n' + content + '\n\n\n\n\n'
+                parser_python_file.write(all)
+                print(len(all))
+            except:
+                pass
+        parser_python_file.close()
+
+    else:
+        print('Парсинг окончен')
+
+hh_parse(base_url, headers)
